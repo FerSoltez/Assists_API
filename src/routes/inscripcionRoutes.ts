fd69d5affab987685ca22740aa2ad18d59c74de2
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-03-19 22:55:12 -0600

    Act 4

--- src/routes/inscripcionRoutes.ts
+++ src/routes/inscripcionRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import inscripcionController from "../controllers/inscripcionController";
 import authMiddleware from "../middleware/authMiddleware";
-import verifyOwnershipClase from "../middleware/verifyOwnershipClase";
 
 const router = Router();
 
@@ -9,6 +8,6 @@ router.post("/inscripciones", authMiddleware, inscripcionController.createInscri
 router.get("/inscripciones", authMiddleware, inscripcionController.getAllInscripciones);
 router.get("/inscripciones/:id", authMiddleware, inscripcionController.getInscripcion);
 router.delete("/inscripciones/:id", authMiddleware, inscripcionController.deleteInscripcion);
-router.get("/inscripciones/clase/:id_clase", authMiddleware, verifyOwnershipClase, inscripcionController.getAlumnosPorClase as any);
+router.get("/inscripciones/clase/:id_clase", authMiddleware, inscripcionController.getAlumnosPorClase as any);
 
 export default router;
