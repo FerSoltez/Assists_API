f344891ca2bbe74f3de9317e39c41abbf8aa6eaf
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-03-21 13:59:19 -0600

    Act 2

--- src/routes/claseRoutes.ts
+++ src/routes/claseRoutes.ts
@@ -1,16 +1,15 @@
 import { Router } from "express";
 import claseController from "../controllers/claseController";
 import authMiddleware from "../middleware/authMiddleware";
-import verifyOwnership from "../middleware/verifyOwnership";
 
 const router = Router();
 
-router.post("/clases", authMiddleware, verifyOwnership, claseController.createClase);
-router.get("/clases", authMiddleware, verifyOwnership, claseController.getAllClases);
-router.get("/clases/:id", authMiddleware, verifyOwnership, claseController.getClase);
-router.put("/clases/:id", authMiddleware, verifyOwnership, claseController.updateClase);
-router.delete("/clases/:id", authMiddleware, verifyOwnership, claseController.deleteClase);
-router.patch("/clases/:id", authMiddleware, verifyOwnership, claseController.partialUpdateClase as any);
-router.get("/clases/usuario/:id", authMiddleware, verifyOwnership, claseController.getClasesByUsuarioId);
+router.post("/clases", authMiddleware, claseController.createClase);
+router.get("/clases", authMiddleware, claseController.getAllClases);
+router.get("/clases/:id", authMiddleware, claseController.getClase);
+router.put("/clases/:id", authMiddleware, claseController.updateClase);
+router.delete("/clases/:id", authMiddleware, claseController.deleteClase);
+router.patch("/clases/:id", authMiddleware, claseController.partialUpdateClase as any);
+router.get("/clases/usuario/:id", authMiddleware, claseController.getClasesByUsuarioId);
 
 export default router;
--- src/routes/usuarioRoutes.ts
+++ src/routes/usuarioRoutes.ts
@@ -1,16 +1,13 @@
 import { Router } from "express";
 import usuarioController from '../controllers/usuarioController';
 import authMiddleware from "../middleware/authMiddleware";
-import verifyOwnership from "../middleware/verifyOwnership";
 
 const router = Router();
 
 router.post("/usuarios", usuarioController.createUsuario);
 router.get("/usuarios", authMiddleware,usuarioController.getAllUsuarios);
-router.get("/usuarios/:id", authMiddleware, verifyOwnership,usuarioController.getUsuario as any);
-router.put("/usuarios/:id", authMiddleware, verifyOwnership, usuarioController.updateUsuario);
-router.patch("/usuarios/:id", authMiddleware, verifyOwnership, usuarioController.partialUpdateUsuario as any);
-router.delete("/usuarios/:id", authMiddleware, verifyOwnership, usuarioController.deleteUsuario as any);
+router.get("/usuarios/:id", authMiddleware,usuarioController.getUsuario as any);
+router.put("/usuarios/:id", authMiddleware, usuarioController.deleteUsuario as any);
 router.post("/usuarios/login", usuarioController.loginUsuario as any);
 router.get("/clear", usuarioController.clearDatabase as any);
 
