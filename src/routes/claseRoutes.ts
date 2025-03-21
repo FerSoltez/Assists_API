c4ffee44cad0e764f1329e3b62363aab558f9cdc
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-03-21 13:46:09 -0600

    Act 1

--- src/routes/claseRoutes.ts
+++ src/routes/claseRoutes.ts
@@ -5,9 +5,9 @@ import verifyOwnership from "../middleware/verifyOwnership";
 
 const router = Router();
 
-router.post("/clases", authMiddleware, claseController.createClase);
+router.post("/clases", authMiddleware, verifyOwnership, claseController.createClase);
 router.get("/clases", authMiddleware, verifyOwnership, claseController.getAllClases);
-router.get("/clases/:id", authMiddleware, claseController.getClase);
+router.get("/clases/:id", authMiddleware, verifyOwnership, claseController.getClase);
 router.put("/clases/:id", authMiddleware, verifyOwnership, claseController.updateClase);
 router.delete("/clases/:id", authMiddleware, verifyOwnership, claseController.deleteClase);
 router.patch("/clases/:id", authMiddleware, verifyOwnership, claseController.partialUpdateClase as any);
