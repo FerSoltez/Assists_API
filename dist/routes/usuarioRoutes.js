9d28e2a87451043a3f043d0f71eeb59e9872aa15
FerSoltez <19090483@alumno.utmetropolitana.edu.mx>
2025-03-24 20:40:16 -0600

    Act 1

--- dist/routes/usuarioRoutes.js
+++ dist/routes/usuarioRoutes.js
@@ -8,7 +8,7 @@ const usuarioController_1 = __importDefault(require("../controllers/usuarioContr
 const authMiddleware_1 = __importDefault(require("../middleware/authMiddleware"));
 const router = (0, express_1.Router)();
 router.post("/usuarios", usuarioController_1.default.createUsuario);
-router.get("/usuarios", authMiddleware_1.default, usuarioController_1.default.getAllUsuarios);
+router.get("/usuarios", usuarioController_1.default.getAllUsuarios);
 router.get("/usuarios/:id", authMiddleware_1.default, usuarioController_1.default.getUsuario);
 router.patch("/usuarios/:id", authMiddleware_1.default, usuarioController_1.default.deleteUsuario);
 router.post("/usuarios/login", usuarioController_1.default.loginUsuario);
